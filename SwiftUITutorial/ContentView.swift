1882f7e09cb3ed4e5e964c3fbd75a6a42a906f42
Marcelo Velásquez <mvelasquezherrera@outlook.com>
2021-06-27 14:57:34 -0500

    Primer commit

---
+++ SwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Marcelo Stefano Velasquez Herrera on 27/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Marcelo Stefano Velasquez Herrera on 27/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
